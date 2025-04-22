1f716b091d0427372f111e81f2b8eb2e938b0b80
RahulRamanDash <dashrahul193@gmail.com>
2025-04-22 17:53:03 +0530

    Added getter & setters for agent entity

--- RealestateApiServer/src/main/java/com/server/RealestateApiServer/Entity/Agent.java
+++ RealestateApiServer/src/main/java/com/server/RealestateApiServer/Entity/Agent.java
@@ -28,47 +28,47 @@ public class Agent {
     @OneToMany(mappedBy = "agent", cascade = CascadeType.ALL)
     private List<Property> properties;
     
-//	public Long getId() {
-//		return id;
-//	}
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getEmail() {
-//		return email;
-//	}
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//	public String getPassword() {
-//		return password;
-//	}
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//	public String getPhone() {
-//		return phone;
-//	}
-//	public void setPhone(String phone) {
-//		this.phone = phone;
-//	}
-//	public Agent() {
-//		super();
-//		// TODO Auto-generated constructor stub
-//	}
-//	public Agent(Long id, String name, String email, String password, String phone, List<Property> properties) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//		this.email = email;
-//		this.password = password;
-//		this.phone = phone;
-//		this.properties = properties;
-//	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Agent() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Agent(Long id, String name, String email, String password, String phone, List<Property> properties) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.properties = properties;
+	}
 }
