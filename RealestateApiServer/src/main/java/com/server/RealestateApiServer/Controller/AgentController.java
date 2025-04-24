5309ccb8c41b6226e540c743a006a1c00107da34
Ayush Samanta <ayushsa@infinite.com>
2025-04-24 18:30:24 +0530

    updated pom file

--- RealestateApiServer/src/main/java/com/server/RealestateApiServer/Controller/AgentController.java
+++ RealestateApiServer/src/main/java/com/server/RealestateApiServer/Controller/AgentController.java
@@ -1,3 +1,4 @@
+
 package com.server.RealestateApiServer.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- RealestateApiServer/src/main/java/com/server/RealestateApiServer/Entity/Agent.java
+++ RealestateApiServer/src/main/java/com/server/RealestateApiServer/Entity/Agent.java
@@ -2,16 +2,18 @@ package com.server.RealestateApiServer.Entity;
 
 import java.util.List;
 
-
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.*;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Agent {
 	@Id
@@ -25,47 +27,5 @@ public class Agent {
     @OneToMany(mappedBy = "agent", cascade = CascadeType.ALL)
     private List<Property> properties;
     
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public Agent() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Agent(Long id, String name, String email, String password, String phone, List<Property> properties) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.phone = phone;
-		this.properties = properties;
-	}
+	
 }
